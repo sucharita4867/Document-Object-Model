002539078679af18bfc6cba84107b9b76b131883
sucharita4867 <sucharitasardar7@gmail.com>
2025-08-30 21:02:58 +0530

    recape the odd consept

---
+++ ES6-intro/let-const.js
@@ -0,0 +1,9 @@
+const price = [34, 45];
+price.push(67);
+console.log(price);
+
+let money = 50;
+money = 60;
+money = 160;
+money = 360;
+console.log(money);
---
+++ ES6-intro/typeChake.js
@@ -0,0 +1,8 @@
+let number = "30";
+console.log(typeof number);
+
+let num = ["raja", "ram", "sita"];
+console.log(typeof num);
+
+let student = { name: "rajib", age: 34 };
+console.log(typeof student);
