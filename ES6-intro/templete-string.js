ea06df00fed27f088c4a60099b437b815f77a1f7
sucharita4867 <sucharitasardar7@gmail.com>
2025-08-30 23:32:08 +0530

    added templete string practice

---
+++ ES6-intro/templete-string.js
@@ -0,0 +1,11 @@
+function numbers(num1, num2) {
+  const result = `total no is ${num1 + num2}`;
+  //   console.log(result);
+}
+numbers(100, 300);
+
+function discount(price) {
+  const output = `this product ${price} discount 20% is ${price * 0.8}`;
+  console.log(output);
+}
+discount(100);
