258d432341fba27a1bd7780d36849233e1d7d9d9
sucharita4867 <sucharitasardar7@gmail.com>
2025-08-30 23:18:37 +0530

    added practice default parameters

---
+++ ES6-intro/default.parameter.js
@@ -0,0 +1,17 @@
+function number(num1 = 0, num2 = 0) {
+  const total = num1 + num2;
+  //   console.log(num1, num2, total);
+}
+number(10);
+
+// some thumb rule of default value . may be useful
+// add , substract --> 0
+/*
+ *multiply --> 1
+ *
+ *string --> ""
+ *
+ *array = []
+ *
+ *object ={}
+ */
