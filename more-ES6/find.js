146e5a3fbf91eac29ec1777e707972623f63c1b7
sucharita4867 <sucharitasardar7@gmail.com>
2025-09-01 21:12:29 +0530

    added fine practice

--- more-ES6/find.js
+++ more-ES6/find.js
@@ -1,3 +1,5 @@
 const numbers = [2, 3, 5, 6, 7, 8, 9, 1];
 const evenNumber = numbers.find((x) => x % 2 === 0);
-console.log(evenNumber);
+// console.log(evenNumber);
+const oddNumber = numbers.find((z) => z % 2 !== 0);
+console.log(oddNumber);
--- more-ES6/reduce.js
+++ more-ES6/reduce.js
@@ -8,14 +8,3 @@ function min(nums) {
 console.log(min([1, 3, 2]));
 const cube = (x) => x * x * x;
 console.log(cube(2));
-const [a, b] = [1, 2, 3, 4, 45, 5];
-console.log(a + b);
-const { x, y, z } = { x: 1, y1: 2, z: 3 };
-console.log(x, y, z);
-const nums = [1, 2, 3, 4, 5];
-let output = nums.filter((n) => n % 2);
-console.log(output);
-const friends = ["Moushumi", "Misha", "Manna", "mimi", "mahiya"];
-
-friends.find((friend) => friend.length == 5);
-console.log(friends);
