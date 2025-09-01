48ece0fbae6667ccb36ed2297fb598a9dd7ee122
sucharita4867 <sucharitasardar7@gmail.com>
2025-09-01 21:09:17 +0530

    added practice filter map

--- more-ES6/filter.js
+++ more-ES6/filter.js
@@ -1,5 +1,7 @@
-const numbers = [2, 3, 5, 6, 7, 8, 9, 1];
+const numbers = [2, 3, 54, 63, 7, 85, 9, 13];
 
 const graterThan10 = numbers.filter((x) => x > 5);
 const even = numbers.filter((num) => num % 2 === 0);
-console.log(even);
+// console.log(even);
+const multiplay = numbers.filter((i) => i < 10);
+console.log(multiplay);
