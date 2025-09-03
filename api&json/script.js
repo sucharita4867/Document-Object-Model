2412f4f698ba860ee13dd06c2229b898bb22a183
sucharita4867 <sucharitasardar7@gmail.com>
2025-09-03 17:51:29 +0530

    added this practice

--- api&json/script.js
+++ api&json/script.js
@@ -1,23 +1,46 @@
-const person = {
-  name: "rabi",
-  age: 34,
-  money: 20000,
-  works: "dev",
-  family: {
-    son: "ishan",
-    girl: "ishani",
-    wife: "paromita",
-  },
+// const person = {
+//   name: "rabi",
+//   age: 34,
+//   money: 20000,
+//   works: "dev",
+//   family: {
+//     son: "ishan",
+//     girl: "ishani",
+//     wife: "paromita",
+//   },
+// };
+// // console.log(typeof people);
+// console.log(person);
+
+// // JSON : js object with notation
+// // JSON.stringify ==> string;
+// // JSON.parse ==> object;
+
+// const personJSON = JSON.stringify(person);
+// console.log(personJSON, typeof personJSON);
+
+// const parseJSON = JSON.parse(personJSON);
+// console.log(parseJSON, typeof parseJSON);
+
+const loadData = () => {
+  fetch("https://jsonplaceholder.typicode.com/todos/1")
+    // promise of response
+    .then((response) => response.json())
+    // Promise of json data
+    .then((json) => console.log(json));
 };
-// console.log(typeof people);
-console.log(person);
 
-// JSON : js object with notation
-// JSON.stringify ==> string;
-// JSON.parse ==> object;
-
-const personJSON = JSON.stringify(person);
-console.log(personJSON, typeof personJSON);
-
-const parseJSON = JSON.parse(personJSON);
-console.log(parseJSON, typeof parseJSON);
+const loadPost = () => {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      displayPost(data);
+    });
+};
+const displayPost = (posts) => {
+  posts.forEach((post) => {
+    console.log(post);
+  });
+};
