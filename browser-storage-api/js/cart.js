0338f98973b6a82f4e6804c1c3b37372c977641b
sucharita4867 <sucharitasardar7@gmail.com>
2025-09-11 22:36:50 +0530

    added today practice

--- browser-storage-api/js/cart.js
+++ browser-storage-api/js/cart.js
@@ -2,11 +2,36 @@ const handleProduct = () => {
   const productEl = document.getElementById("product");
   const QuantityEl = document.getElementById("Quantity");
   const product = productEl.value;
-  const quantity = QuantityEl.value;
-  console.log("product Add", product, quantity);
+  const quantity = parseInt(QuantityEl.value);
+  //   console.log("product Add", product, quantity);
+
+  displayProduct(product, quantity);
+  addProductToCart(product, quantity);
 
   productEl.value = "";
   QuantityEl.value = "";
+};
+
+const getCart = () => {
+  let cart = {};
+  const cartJSON = localStorage.getItem("cart");
+  if (cartJSON) {
+    cart = JSON.parse(cartJSON);
+  }
+  return cart;
+};
+
+const addProductToCart = (product, quantity) => {
+  const cart = getCart();
+  if (cart[product]) {
+    cart[product] = cart[product] + quantity;
+  } else {
+    cart[product] = quantity;
+  }
+
+  console.log("cart", cart);
+  const cartJSON = JSON.stringify(cart);
+  localStorage.setItem("cart", cartJSON);
 };
 
 const displayProduct = (product, quantity) => {
@@ -16,3 +41,28 @@ const displayProduct = (product, quantity) => {
   const ul = document.getElementById("product-container");
   ul.append(li);
 };
+
+//display product from stored local storage
+
+const displayStoredProducts = () => {
+  const cart = getCart();
+  for (const product in cart) {
+    const quantity = cart[product];
+    console.log(product, quantity);
+    displayProduct(product, quantity);
+  }
+};
+displayStoredProducts();
+/**
+ *   To save object in the local storage
+ *  1. convert the object to JSON string by using JSON . stringify
+ *  2. localstorage.setItem()
+ ***/
+
+/**
+ *  to get object/array from the local storage
+ * 1. get the item from the local storage and it will be in JSON string
+ * 2. convert the JSON string to js object by using JSON. parse
+ *
+ *
+ * */
