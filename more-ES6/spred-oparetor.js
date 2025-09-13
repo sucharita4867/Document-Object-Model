e27ce3ac7737e9fbcc8dae43f4193557951ba1b4
sucharita4867 <sucharitasardar7@gmail.com>
2025-09-13 21:16:07 +0530

    added practice map and find

--- more-ES6/spred-oparetor.js
+++ more-ES6/spred-oparetor.js
@@ -2,4 +2,10 @@ const numbers = [3, 7, 3, 9, 9, 5, 8, 3];
 // console.log(...numbers);
 const even = numbers.filter((num) => num % 2 == 0);
 const oddNum = numbers.filter((num) => num % 2 === 1);
-console.log(oddNum);
+// console.log(oddNum);
+// numbers.forEach((num) => num > 10)
+const array = numbers.map((num) => num + 10);
+// console.log(array);
+const num = numbers.find((number) => number < 10);
+console.log(num);
+// const double = numbers.map((x) => x * 4);
